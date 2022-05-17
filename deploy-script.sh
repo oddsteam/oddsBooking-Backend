faf7315b2ace0153ef78e296bb25cc3b699d46c6
jerry19857 <piyapongvisitsin@gmail.com>
2022-05-17 15:47:02 +0700

    [dome] refactor: config deploy-script

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker login -u ap-southeast-2@H97WABNOA1NBRPW8INUL -p aa275bca967ab0e83dccf3c57efb23ff981d9cd8ae4c66089d4aa25cdf971292 ${REGISTRY}
+docker login -u ap-southeast-2@OA4R6SQSJDS6O5TPXWUJ -p 092929273c8458b0141bdca0a6475a3f3103eb3f4fa57b4a5405635828bc4c9a ${REGISTRY}
 docker compose pull
 docker compose down
 docker compose up -d
