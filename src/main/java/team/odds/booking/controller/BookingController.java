df99b79e06559c4ea5c500cadbe562e4f669fcf9
Chandara-Sin <>
2022-05-26 13:59:54 +0700

    refactor: clean code and adjust project structure

--- src/main/java/team/odds/booking/controller/BookingController.java
+++ src/main/java/team/odds/booking/controller/BookingController.java
@@ -1,41 +1,32 @@
 package team.odds.booking.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team.odds.booking.model.Booking;
+import team.odds.booking.model.dto.BookingDto;
 import team.odds.booking.service.BookingService;
 
-import java.util.List;
-
+@AllArgsConstructor
 @RequestMapping("/v1/booking")
 @RestController
 public class BookingController {
 
-    private final BookingService bookingService;
-
-    public BookingController(BookingService bookingService) {
-        this.bookingService = bookingService;
-    }
+    final BookingService bookingService;
 
     @GetMapping(value = "/{bookingId}")
     public ResponseEntity<Booking> getBooking(@PathVariable(value = "bookingId") String bookingId) throws Exception {
         return new ResponseEntity<>(bookingService.getBooking(bookingId), HttpStatus.OK);
     }
 
-    @GetMapping()
-    public ResponseEntity<List<Booking>> getBookingList() throws Exception {
-        return new ResponseEntity<>(bookingService.getBookingList(), HttpStatus.OK);
-    }
-
     @PostMapping()
-    public ResponseEntity<Booking> addBooking(@RequestBody Booking dataRequest) throws Exception {
+    public ResponseEntity<Booking> addBooking(@RequestBody BookingDto dataRequest) throws Exception {
         return new ResponseEntity<>(bookingService.addBooking(dataRequest), HttpStatus.CREATED);
     }
 
     @PutMapping(value = "/{bookingId}")
-    public ResponseEntity<Booking> updateBooking(@PathVariable(value = "bookingId") String bookingId, @RequestBody Booking dataRequest) throws Exception {
+    public ResponseEntity<Booking> updateBooking(@PathVariable(value = "bookingId") String bookingId, @RequestBody BookingDto dataRequest) throws Exception {
         return new ResponseEntity<>(bookingService.updateBooking(bookingId, dataRequest), HttpStatus.OK);
     }
 
