bd37732c6553063b82cd26be195e309bf41e0a0c
Chandara-Sin <>
2022-05-27 09:18:50 +0700

    refactor: change to mail phi roof

--- src/main/java/team/odds/booking/service/MailSenderService.java
+++ src/main/java/team/odds/booking/service/MailSenderService.java
@@ -45,7 +45,7 @@ public record MailSenderService(JavaMailSender mailSender, TemplateEngine templa
 
         MimeMessage mailCompose = this.mailSender.createMimeMessage();
         var mailComposeParts = new MimeMessageHelper(mailCompose, true, "UTF-8");
-        mailComposeParts.setTo("phum.project@gmail.com");
+        mailComposeParts.setTo("roof@odds.team");
         mailComposeParts.setSubject("รายละเอียดการจอง");
         mailComposeParts.setFrom(new InternetAddress("odds.molamola@gmail.com", "odds-booking"));
 
