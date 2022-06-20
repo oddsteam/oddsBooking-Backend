4d4f2d5451996c3d77565706fb3ac53374b75d84
Chandara-Sin <>
2022-06-20 16:57:59 +0700

    feat: add dotenv

--- src/main/java/team/odds/booking/service/MailSendinblueService.java
+++ src/main/java/team/odds/booking/service/MailSendinblueService.java
@@ -1,24 +1,11 @@
 package team.odds.booking.service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import team.odds.booking.constants.MailSender;
 import team.odds.booking.model.Booking;
 
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 import sibApi.TransactionalEmailsApi;
-import team.odds.booking.model.Booking;
-import team.odds.booking.util.HelpersUtil;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Field;
-import java.time.LocalDateTime;
 
 import sendinblue.*;
 import sendinblue.auth.*;
@@ -37,7 +24,9 @@ public class MailSendinblueService implements MailSender {
 
             // Configure API key authorization: api-key
             ApiKeyAuth apiKey = (ApiKeyAuth) defaultClient.getAuthentication("api-key");
-            apiKey.setApiKey("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");
+            Dotenv dotenv = Dotenv.load();
+            apiKey.setApiKey(dotenv.get("SENDINBLUE_TOKEN"));
+            System.out.println(dotenv.get("SENDINBLUE_TOKEN"));
             // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
             //apiKey.setApiKeyPrefix("Token");
 
@@ -85,7 +74,8 @@ public class MailSendinblueService implements MailSender {
 
         // Configure API key authorization: api-key
         ApiKeyAuth apiKey = (ApiKeyAuth) defaultClient.getAuthentication("api-key");
-        apiKey.setApiKey("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");
+        Dotenv dotenv = Dotenv.load();
+        apiKey.setApiKey(dotenv.get("SENDINBLUE_TOKEN"));
         // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
         //apiKey.setApiKeyPrefix("Token");
 
