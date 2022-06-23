174896f10f32f2b8ff180d487b531139e276e0a2
Chandara-Sin <>
2022-06-23 16:57:12 +0700

    refactor: add custom booking response

--- src/main/java/team/odds/booking/controller/BookingController.java
+++ src/main/java/team/odds/booking/controller/BookingController.java
@@ -1,8 +1,11 @@
 package team.odds.booking.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team.odds.booking.model.Booking;
+import team.odds.booking.model.BookingResponse;
 import team.odds.booking.model.dto.BookingDto;
 import team.odds.booking.service.BookingService;
 
@@ -14,18 +17,23 @@ public class BookingController {
     final BookingService bookingService;
 
     @GetMapping(value = "/{bookingId}")
-    public Booking getBooking(@PathVariable(value = "bookingId") String bookingId) {
-        return bookingService.getBooking(bookingId);
+    public ResponseEntity<BookingResponse<Booking>> getBooking(@PathVariable(value = "bookingId") String bookingId) {
+        var res = new BookingResponse<>(HttpStatus.OK.value(), bookingService.getBooking(bookingId));
+        return ResponseEntity.ok().body(res);
     }
 
     @PostMapping()
-    public Booking addBooking(@RequestBody BookingDto dataRequest) {
-        return bookingService.addBooking(dataRequest);
+    public ResponseEntity<BookingResponse<Booking>> addBooking(@RequestBody BookingDto dataRequest) {
+        var res = new BookingResponse<>(HttpStatus.CREATED.value(), bookingService.addBooking(dataRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
     }
 
     @PutMapping(value = "/{bookingId}")
-    public Booking updateBooking(@PathVariable(value = "bookingId") String bookingId, @RequestBody BookingDto dataRequest) {
-        return bookingService.updateBooking(bookingId, dataRequest);
+    public ResponseEntity<BookingResponse<Booking>> updateBooking(
+            @PathVariable(value = "bookingId") String bookingId,
+            @RequestBody BookingDto dataRequest) {
+        var res = new BookingResponse<>(HttpStatus.OK.value(), bookingService.updateBooking(bookingId, dataRequest));
+        return ResponseEntity.ok().body(res);
     }
 
 }
