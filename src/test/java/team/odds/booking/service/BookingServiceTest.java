719a64ececafd49681dcf19ea33eb1ebc94c36d8
Chandara-Sin <>
2022-06-24 09:12:42 +0700

    refactor: add helper function and update test

--- src/test/java/team/odds/booking/service/BookingServiceTest.java
+++ src/test/java/team/odds/booking/service/BookingServiceTest.java
@@ -1,10 +1,12 @@
 package team.odds.booking.service;
 
 import org.junit.jupiter.api.Test;
+import team.odds.booking.exception.DataNotFound;
 import team.odds.booking.model.Booking;
 import team.odds.booking.model.dto.BookingDto;
 import team.odds.booking.model.mapper.BookingMapper;
 import team.odds.booking.repository.BookingRepository;
+import team.odds.booking.util.HelpersUtil;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -24,19 +26,12 @@ public class BookingServiceTest {
     @Test
     void getBooking() {
         // Arrange
-        var dateTime = LocalDateTime.now().toString();
+        var localDateTimeNow = LocalDateTime.now();
         var booking = new Booking(
-                "1234",
-                "Milk",
-                "milkeyway124@gmail.com",
-                "0640230334",
-                "Neon",
-                "Booking Event",
-                LocalDateTime.parse(dateTime),
-                LocalDateTime.parse(dateTime),
-                false,
-                LocalDateTime.parse(dateTime),
-                LocalDateTime.parse(dateTime)
+                "1234", "Taliw", "taliw78@gmail.com",
+                "0640230334", "Neon", "Organize Event",
+                localDateTimeNow, localDateTimeNow, false,
+                localDateTimeNow, localDateTimeNow
         );
         String bookingId = booking.getId();
         when(bookingRepository.findById(bookingId)).thenReturn(Optional.of(booking));
@@ -54,7 +49,7 @@ public class BookingServiceTest {
         var bookingId = "1-jfd234";
         when(bookingRepository.findById(bookingId)).thenReturn(Optional.empty());
         // Act
-        var exception = assertThrows(Exception.class, () -> bookingService.getBooking(bookingId));
+        var exception = assertThrows(DataNotFound.class, () -> bookingService.getBooking(bookingId));
         // Assert
         assertThat(exception.getMessage()).isEqualTo("Booking not found with this id : " + bookingId);
         verify(bookingRepository).findById(bookingId);
@@ -63,37 +58,57 @@ public class BookingServiceTest {
     @Test
     void addBooking() {
         // Arrange
-        var dateTime = "2020-08-21T04:21";
-        var bookingPostDto = new BookingDto(
-                "Milk",
-                "milkeyway124@gmail.com",
-                "0640230334",
-                "Neon",
-                "Booking Event",
-                dateTime,
-                dateTime,
+        var startDate = LocalDateTime.of(2022, 8, 9, 10, 34);
+        var endDate = LocalDateTime.of(2022, 8, 10, 10, 34);
+        var bookingReq = new BookingDto(
+                "Taliw", "taliw78@gmail.com",
+                "0640230334", "Neon", "Organize Event",
+                HelpersUtil.dateTimeFormatGeneral(startDate),
+                HelpersUtil.dateTimeFormatGeneral(endDate),
                 false
         );
         var bookingRes = new Booking(
-                "1234",
-                "Milk",
-                "milkeyway124@gmail.com",
-                "0640230334",
-                "Neon",
-                "Booking Event",
-                LocalDateTime.parse(dateTime),
-                LocalDateTime.parse(dateTime),
-                false,
-                LocalDateTime.parse(dateTime),
-                LocalDateTime.parse(dateTime)
+                "1234", "Taliw", "taliw78@gmail.com",
+                "0640230334", "Neon", "Organize Event",
+                startDate, endDate, false,
+                LocalDateTime.now(), LocalDateTime.now()
         );
+        when(bookingMapper.toBooking(bookingReq)).thenReturn(bookingRes);
         when(bookingRepository.save(any(Booking.class))).thenReturn(bookingRes);
-        when(bookingMapper.toBooking(any(BookingDto.class))).thenReturn(bookingRes);
         // Act
-        var addBooking = bookingService.addBooking(bookingPostDto);
+        var addBooking = bookingService.addBooking(bookingReq);
         // Assert
         assertThat(addBooking.getId()).isEqualTo("1234");
         assertThat(addBooking.getStatus()).isEqualTo(false);
+        verify(bookingRepository).save(any(Booking.class));
+    }
+
+    @Test
+    void updateBooking() {
+        // Arrange
+        var bookingReqId = "1234";
+        var startDate = LocalDateTime.of(2022, 8, 9, 10, 34);
+        var endDate = LocalDateTime.of(2022, 8, 10, 10, 34);
+        var bookingReq = new BookingDto(
+                "Taliw", "taliw78@gmail.com",
+                "0640230334", "Neon", "Organize Event",
+                HelpersUtil.dateTimeFormatGeneral(startDate),
+                HelpersUtil.dateTimeFormatGeneral(endDate), true
+        );
+        var bookingRes = new Booking(
+                "1234", "Pahn", "pahn78@gmail.com",
+                "0640230334", "Neon", "Organize Event",
+                startDate, endDate, true,
+                LocalDateTime.now(), LocalDateTime.now()
+        );
+        when(bookingRepository.findById(bookingReqId)).thenReturn(Optional.of(bookingRes));
+        when(bookingMapper.toBooking(bookingReq)).thenReturn(bookingRes);
+        when(bookingRepository.save(any(Booking.class))).thenReturn(bookingRes);
+        // Act
+        var addBooking = bookingService.updateBooking(bookingReqId, bookingReq);
+        // Assert
+        assertThat(addBooking.getId()).isEqualTo("1234");
+        assertThat(addBooking.getStatus()).isEqualTo(true);
         verify(bookingRepository).save(any(Booking.class));
     }
 }
---
+++ src/test/java/team/odds/booking/util/HelpersUtilTest.java
@@ -0,0 +1,37 @@
+package team.odds.booking.util;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelpersUtilTest {
+
+    @Test
+    void bookingDateExpired() {
+        try (var mockedStatic = Mockito.mockStatic(LocalDateTime.class, Mockito.CALLS_REAL_METHODS)) {
+            // Arrange
+            var bookingDate = LocalDateTime.of(2022, 8, 10, 8, 0);
+            // Mock LocalDateTime.now()
+            var dateTimeNow = LocalDateTime.of(2022, 8, 11, 8, 0);
+            mockedStatic.when(LocalDateTime::now).thenReturn(dateTimeNow);
+            // Act
+            boolean isExpired = HelpersUtil.checkBookingDateExpired(bookingDate);
+            // Assert
+            assertThat(isExpired).isTrue();
+        }
+    }
+
+    @Test
+    void bookingDateGeneralFormat() {
+        // Arrange
+        var bookingDate = LocalDateTime.of(2022, 8, 10, 8, 0);
+        var bookingDateRes = "2022-08-10 08:00";
+        // Act
+        String bookingDateFormat = HelpersUtil.dateTimeFormatGeneral(bookingDate);
+        // Assert
+        assert (bookingDateFormat).equals(bookingDateRes);
+    }
+}
